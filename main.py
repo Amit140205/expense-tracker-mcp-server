be20e6ac1391dd238aee1488169cbe96df00e1c5
Amit140205 <rumaamitmondal340@gmail.com>
2026-03-28 12:38:06 +0530

    making db writable through cluade

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import tempfile
 # making writable using claude-desktop
 TEMP_DIR=tempfile.gettempdir()
 
-DB_PATH = os.path.join(os.path.dirname(__file__), "expenses.db")
+DB_PATH = os.path.join(TEMP_DIR, "expenses.db")
 CATEGORIES_PATH = os.path.join(os.path.dirname(__file__), "categories.json")
 
 mcp = FastMCP(name="ExpenseTracker")
